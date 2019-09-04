b99840bf0e9dd3bc5b51de3ba07308ea06428cf6
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2019-09-04 18:34:51 +0200

    Setup SementicUi

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
+import SuiVue from 'semantic-ui-vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
 
+
 Vue.config.productionTip = false;
+
+Vue.use(SuiVue);
 
 new Vue({
   router,
