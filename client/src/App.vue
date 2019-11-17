ee0c37664fd147fcef0e17cc0b6ae08a7fec7cb9
stephane de la torre <s.dlt@hotmail.fr>
2019-11-17 13:17:41 +0100

    Refactorize navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,4 +17,7 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background: #f0f0f0;
+}
 </style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <b-navbar>
+    <b-navbar variant="light shadow-sm">
       <b-navbar-brand href="/">
         <i class="fas fa-dungeon"></i> Raid Scheduler
       </b-navbar-brand>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-button to="/login" variant="primary">Login</b-button>
+          <b-button to="/signup" variant="primary">Sign Up</b-button>
+          &nbsp;
+          <b-button to="/login" variant="primary">Log in</b-button>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
